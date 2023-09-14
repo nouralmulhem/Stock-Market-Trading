da1d245bd2fafebd94497627ccbb4bd95dfe4ad0
Nour Almulhem <nouralmulhem@gmail.com>
2023-09-14 11:24:57 +0300

    [FEAT] add dashboard page

--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,16 @@
-
 import Landing from "./pages/Landing";
 import Authentication from "./pages/Authentication";
-
+import Dashboard from "./pages/Dashboard";
 
 type Routes = {
   name: string;
   key: string;
   icon?: JSX.Element;
-  route: string,
-  component: React.FunctionComponent<any>,
-}
+  route: string;
+  component: React.FunctionComponent<any>;
+};
 
-const routes : Routes[] = [
+const routes: Routes[] = [
   {
     name: "Authentication",
     key: "authentication",
@@ -24,46 +23,12 @@ const routes : Routes[] = [
     route: "/Landing",
     component: Landing,
   },
-  // {
-  //   type: "collapse",
-  //   name: "Dashboard",
-  //   key: "dashboard",
-  //   // icon: <Icon fontSize="small">dashboard</Icon>,
-  //   route: "/dashboard",
-  //   component: Dashboard,
-  // },
-  // {
-  //   type: "collapse",
-  //   name: "Tables",
-  //   key: "tables",
-  //   // icon: <Icon fontSize="small">table_view</Icon>,
-  //   route: "/tables",
-  //   component: Tables,
-  // },
-  // {
-  //   type: "collapse",
-  //   name: "Billing",
-  //   key: "billing",
-  //   // icon: <Icon fontSize="small">receipt_long</Icon>,
-  //   route: "/billing",
-  //   component: Billing,
-  // },
-  // {
-  //   type: "collapse",
-  //   name: "Notifications",
-  //   key: "notifications",
-  //   // icon: <Icon fontSize="small">notifications</Icon>,
-  //   route: "/notifications",
-  //   component: Notifications,
-  // },
-  // {
-  //   type: "collapse",
-  //   name: "Profile",
-  //   key: "profile",
-  //   // icon: <Icon fontSize="small">person</Icon>,
-  //   route: "/profile",
-  //   component: Profile,
-  // },
+  {
+    name: "Dashboard",
+    key: "dashboard",
+    route: "/dashboard",
+    component: Dashboard,
+  },
 ];
 
 export default routes;
