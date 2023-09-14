e2de3e4bca0cbb14e7d765ab3b29f356f028b3d3
Nour Almulhem <nouralmulhem@gmail.com>
2023-09-14 11:20:50 +0300

    [FEAT] setting the routing and addint footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,58 @@
-import React from 'react';
-import './App.css';
+import { useEffect } from "react";
+
+// react-router components
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+
+// @mui material components
+import { ThemeProvider } from "@mui/material/styles";
+
+// Material Dashboard 2 React routes
+import routes from "./routes";
+
+// Material theme
+import theme from "./styles/theme";
+import Footer from "./components/Footer/Footer";
+
+// Cookies
+import { CookiesProvider } from "react-cookie";
 
 function App() {
+  const { pathname } = useLocation();
+
+  // Setting page scroll to 0 when changing the route
+  useEffect(() => {
+    document.documentElement.scrollTop = 0;
+  }, [pathname]);
+
+  const getRoutes = (allRoutes: any) =>
+    allRoutes.map((route: any) => {
+      if (route.collapse) {
+        return getRoutes(route.collapse);
+      }
+
+      if (route.route) {
+        return (
+          <Route
+            path={route.route}
+            Component={route.component}
+            key={route.key}
+          />
+        );
+      }
+
+      return null;
+    });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CookiesProvider>
+        <Routes>
+          {getRoutes(routes)}
+          <Route path="*" element={<Navigate to="/landing" />} />
+        </Routes>
+        <Footer />
+      </CookiesProvider>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,32 @@
+import React, { FC, ReactElement } from "react";
+import { Box, Container, Grid, Typography } from "@mui/material";
+
+export const Footer: FC = (): ReactElement => {
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        paddingBottom: "1rem",
+        bottom: 0,
+        position: "absolute",
+      }}
+    >
+      <Container maxWidth="lg">
+        <Grid container direction="column" alignItems="center">
+          <Grid item xs={12}>
+            <Typography color="white" variant="h5">
+              React Starter App
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography color="#1052c1" variant="subtitle1">
+              {`${new Date().getFullYear()} | React | Material UI | React Router`}
+            </Typography>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
+};
+
+export default Footer;
