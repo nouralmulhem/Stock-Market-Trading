5123d9aef00b7142ae61a37ff5f0df099415e73b
Nour Almulhem <nouralmulhem@gmail.com>
2023-09-15 21:44:49 +0300

    [FIX] trying fix images issue

--- src/components/Authentication/Container/styles.ts
+++ src/components/Authentication/Container/styles.ts
@@ -5,7 +5,7 @@ export const AuthContainer = styled(Box)(() => ({
   height: "100vh",
   boxShadow: "none",
   background:
-    "linear-gradient(195deg, rgba(50, 58, 84, 0.6), rgba(26, 32, 53, 0.6)) center center / cover no-repeat, url(/bg.jpeg) transparent",
+    "linear-gradient(195deg, rgba(50, 58, 84, 0.6), rgba(26, 32, 53, 0.6)) center center / cover no-repeat, url(/public/bg.jpeg) transparent",
   backgroundPosition: "center",
   backgroundRepeat: "no-repeat",
 }));
